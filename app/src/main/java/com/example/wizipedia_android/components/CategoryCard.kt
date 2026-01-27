3ea1d1f18d1489cbc8fc52f5abbeecd227f88b77
Facundo <faquidg@hotmail.com>
2026-01-27 17:29:30 +0100

    chore: remove unnecessary imports

--- app/src/main/java/com/example/wizipedia_android/components/CategoryCard.kt
+++ app/src/main/java/com/example/wizipedia_android/components/CategoryCard.kt
@@ -8,17 +8,14 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.wizipedia_android.enums.Category
-import com.example.wizipedia_android.ui.colors.createGradientColor
 
 @Composable
 fun CategoryCard(
