a17125d1b6b48329f58e0d19bdf952891ac73d18
Raul Catalinas <raulcatalinas@gmail.com>
2026-01-30 00:46:20 +0100

    chore: add size customization

--- app/src/main/java/com/example/wizipedia_android/components/RenderLocalImage.kt
+++ app/src/main/java/com/example/wizipedia_android/components/RenderLocalImage.kt
@@ -10,17 +10,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun RenderLocalImage(
     @DrawableRes drawableResId: Int,
     modifier: Modifier = Modifier,
-    colorTint: Color = Color.White
+    colorTint: Color = Color.White,
+    size: Dp = 25.dp
 ) {
     Icon(
         painter = painterResource(drawableResId),
-        modifier = modifier.size(25.dp),
+        modifier = modifier.size(size),
         contentDescription = "Category icon",
         tint = colorTint
     )
